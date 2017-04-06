8a9a7e1a40bdebcf013c33f9743585474f6a2235
mrmiywj <jsyangwenjie@gmail.com>
2017-04-06 22:35:32 +0800

    lab5 done

--- proj2/editor/Editor.java
+++ proj2/editor/Editor.java
@@ -1,13 +1,152 @@
 package editor;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.VPos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 
 public class Editor extends Application {
+    private final Rectangle textBoundingBox;
+
+    private static final int WINDOW_WIDTH = 500;
+    private static final int WINDOW_HEIGHT = 500;
+
+    public Editor() {
+        // Create a rectangle to surround the text that gets displayed.  Initialize it with a size
+        // of 0, since there isn't any text yet.
+        textBoundingBox = new Rectangle(0, 0);
+    }
+
+    private class KeyEventHandler implements EventHandler<KeyEvent> {
+        int textCenterX;
+        int textCenterY;
+
+        private static final int STARTING_TEXT_POSITION_X = 0;
+        private static final int STARTING_TEXT_POSITION_Y = 0;
+        private static final int STARTING_FONT_SIZE = 14;
+
+        /** The Text to display on the screen. */
+        private Text displayText = new Text(STARTING_TEXT_POSITION_X, STARTING_TEXT_POSITION_Y, "");
+        private int fontSize = STARTING_FONT_SIZE;
+
+        private String fontName = "Verdana";
+
+        KeyEventHandler(final Group root, int windowWidth, int windowHeight) {
+            textCenterX = 0;
+            textCenterY = 0;
+
+            // Initialize some empty text and add it to root so that it will be displayed.
+            displayText = new Text(textCenterX, textCenterY, "");
+            // Always set the text origin to be VPos.TOP! Setting the origin to be VPos.TOP means
+            // that when the text is assigned a y-position, that position corresponds to the
+            // highest position across all letters (for example, the top of a letter like "I", as
+            // opposed to the top of a letter like "e"), which makes calculating positions much
+            // simpler!
+            displayText.setTextOrigin(VPos.TOP);
+            displayText.setFont(Font.font(fontName, fontSize));
+            root.getChildren().add(displayText);
+        }
+
+        @Override
+        public void handle(KeyEvent keyEvent) {
+            if (keyEvent.getEventType() == KeyEvent.KEY_TYPED) {
+                // Use the KEY_TYPED event rather than KEY_PRESSED for letter keys, because with
+                // the KEY_TYPED event, javafx handles the "Shift" key and associated
+                // capitalization.
+                String characterTyped = keyEvent.getCharacter();
+                if (characterTyped.length() > 0 && characterTyped.charAt(0) != 8) {
+                    // Ignore control keys, which have zero length, as well as the backspace
+                    // key, which is represented as a character of value = 8 on Windows.
+                    displayText.setText(displayText.getText() + characterTyped);
+                    keyEvent.consume();
+                }
+
+                centerTextAndUpdateBoundingBox();
+            } else if (keyEvent.getEventType() == KeyEvent.KEY_PRESSED) {
+                // Arrow keys should be processed using the KEY_PRESSED event, because KEY_PRESSED
+                // events have a code that we can check (KEY_TYPED events don't have an associated
+                // KeyCode).
+                KeyCode code = keyEvent.getCode();
+                if (code == KeyCode.UP) {
+                    fontSize += 5;
+                    displayText.setFont(Font.font(fontName, fontSize));
+                    centerTextAndUpdateBoundingBox();
+                } else if (code == KeyCode.DOWN) {
+                    fontSize = Math.max(0, fontSize - 5);
+                    displayText.setFont(Font.font(fontName, fontSize));
+                    centerTextAndUpdateBoundingBox();
+                } else if (code == KeyCode.BACK_SPACE) {
+                    String text = displayText.getText();
+                    displayText.setText(text.substring(0, text.length() - 1));
+                    centerTextAndUpdateBoundingBox();
+                }
+            }
+        }
+
+        private void centerTextAndUpdateBoundingBox() {
+            // Figure out the size of the current text.
+            double textHeight = displayText.getLayoutBounds().getHeight();
+            double textWidth = displayText.getLayoutBounds().getWidth();
+
+            // Calculate the position so that the text will be centered on the screen.
+            double textTop = 0;
+            double textLeft = 0;
+
+            // Re-position the text.
+            displayText.setX(textLeft);
+            displayText.setY(textTop);
+
+            // Re-size and re-position the bounding box.
+            textBoundingBox.setHeight(textHeight);
+            textBoundingBox.setWidth(textWidth);
+
+            // For rectangles, the position is the upper left hand corner.
+            textBoundingBox.setX(textLeft);
+            textBoundingBox.setY(textTop);
+            // Many of the JavaFX classes have implemented the toString() function, so that
+            // they print nicely by default.
+            System.out.println("Bounding box: " + textBoundingBox);
+
+            // Make sure the text appears in front of the rectangle.
+            displayText.toFront();
+        }
+    }
+
     @Override
     public void start(Stage primaryStage) {
+        // Create a Node that will be the parent of all things displayed on the screen.
+        Group root = new Group();
+        // The Scene represents the window: its height and width will be the height and width
+        // of the window displayed.
+        Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT, Color.WHITE);
 
+        // To get information about what keys the user is pressing, create an EventHandler.
+        // EventHandler subclasses must override the "handle" function, which will be called
+        // by javafx.
+        EventHandler<KeyEvent> keyEventHandler =
+                new KeyEventHandler(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+        // Register the event handler to be called for all KEY_PRESSED and KEY_TYPED events.
+        scene.setOnKeyTyped(keyEventHandler);
+        scene.setOnKeyPressed(keyEventHandler);
+
+        primaryStage.setTitle("Single Letter Display Simple");
+
+        // This is boilerplate, necessary to setup the window where things are displayed.
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
