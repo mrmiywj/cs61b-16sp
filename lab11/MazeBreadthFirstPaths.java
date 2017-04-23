b5a3bb8be276d793609feb15a873ae3e16519298
mrmiywj <jsyangwenjie@gmail.com>
2017-04-23 17:16:38 +0800

    lab11 done

--- lab11/MazeBreadthFirstPaths.java
+++ lab11/MazeBreadthFirstPaths.java
@@ -1,3 +1,4 @@
+import java.util.LinkedList;
 import java.util.Observable;
 /**
  *  @author Josh Hug
@@ -9,20 +10,53 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    LinkedList<Integer> queue;
+    int s;
+    int t;
+    private boolean targetFound = false;
+    Maze maze;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        queue = new LinkedList<Integer>();
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     /** Conducts a breadth first search of the maze starting at vertex x. */
     private void bfs(int s) {
         /* Your code here. */
+        marked[s] = true;
+        queue.addLast(s);
+
+        announce();
+
+        while (!queue.isEmpty()) {
+            int now = queue.pollFirst();
+            if (now == t) {
+                return;
+            }
+            for (int w: maze.adj(now)) {
+                if (!marked[w]) {
+                    marked[w] = true;
+                    queue.addLast(w);
+
+                    distTo[w] = distTo[now] + 1;
+                    announce();
+                    edgeTo[w] = now;
+                }
+            }
+        }
+
     }
 
 
     @Override
     public void solve() {
-        // bfs(s);
+         bfs(s);
     }
 }
 
--- lab11/MazeCycles.java
+++ lab11/MazeCycles.java
@@ -10,12 +10,34 @@ public class MazeCycles extends MazeExplorer {
     public boolean[] marked;
     */
 
+
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
+    }
+
+    private boolean isCyclicUntil(int v, int parent) {
+        marked[v] = true;
+        edgeTo[v] = parent;
+        for (int w: maze.adj(v)) {
+            if (!marked[w]){
+                if (isCyclicUntil(w, v)) {
+                    return true;
+                }
+            } else if (w != parent) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public void solve() {
+        for (int i = 0; i < maze.V(); i++) {
+            if (!marked[i])
+                if (isCyclicUntil(i, -1))
+                    return;
+        }
     }
 } 
 
